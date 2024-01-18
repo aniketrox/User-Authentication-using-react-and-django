5b06a1c972f72ac0637380e4214810a5e72dc783
Aniket Guchhait <aniket.emailme@gmail.com>
2024-01-19 04:29:22 +0530

    Add files via upload

---
+++ frontend/build/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import Activate from './containers/Activate';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+import Facebook from './containers/Facebook';
+import Google from './containers/Google';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Layout from './hocs/Layout';
+
+const App = () => (
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/signup' component={Signup} />
+                    <Route exact path='/facebook' component={Facebook} />
+                    <Route exact path='/google' component={Google} />
+                    <Route exact path='/reset-password' component={ResetPassword} />
+                    <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+                    <Route exact path='/activate/:uid/:token' component={Activate} />
+                </Routes>
+            </Layout>
+        </Router>
+    </Provider>
+);
+
+export default App;
