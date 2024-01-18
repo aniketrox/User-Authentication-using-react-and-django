3e617ec79416ffa68b28705dce5e5bec77500fc2
Aniket Guchhait <aniket.emailme@gmail.com>
2024-01-19 04:25:34 +0530

    Add files via upload

---
+++ backend/accounts/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    user_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = ['email']
+
+    def get_user_name(self):
+        return self.user_name
+
+    # def get_short_name(self):
+    #     return self.first_name
+    
+    def __str__(self):
+        return self.email
